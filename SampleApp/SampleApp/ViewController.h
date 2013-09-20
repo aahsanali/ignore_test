6f8dc3c056e82b29ead1dba40f2ea237f0cff2ca
Ahsan <naveed.ahsan@coeus-solutions.de>
2013-09-20 11:29:38 +0500

    initial project setup

---
+++ SampleApp/SampleApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SampleApp
+//
+//  Created by Ahsan on 9/20/13.
+//  Copyright (c) 2013 Coeus Solutions GmbH. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
